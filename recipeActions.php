19cf8fa76117d6cd45e0e0fcf9fac8e78ff08484
JoannaLilja <33461707+JoannaLilja@users.noreply.github.com>
2017-11-20 23:15:10 +0100

    Update recipeActions.php

--- recipeActions.php
+++ recipeActions.php
@@ -13,6 +13,8 @@ if ($dbcon->connect_errno)
 
 $type_id = $dbcon->query('SELECT * FROM recipe WHERE name="'.$type.'"')->fetch_assoc()['id'];
 
+var_dump($user_id);
+
 $dbcon->query(
   'INSERT INTO comments (user_id, type, comment) VALUES ('. $user_id .', ' . $type_id . ', "' . $comment . '")');
 
@@ -20,4 +22,4 @@ $dbcon->close();
 
 header('Location: '.$type.'.php');
 
- ?>
+?>
