592eb97ec3d41c3c716da4f359d0c9bd4c346c5b
JoannaLilja <33461707+JoannaLilja@users.noreply.github.com>
2017-11-20 23:53:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
-    <link rel = "stylesheet" type = "text/css" href = "styles/reset.css">
     <link rel = "stylesheet" type = "text/css" href = "styles/stylesheet.css">
 
 </head>
@@ -28,7 +27,7 @@
                             <li><a href="calendar.php" style = "color: rgb(0,0,0)">Calendar</a></li>
                             <li><a href="meatballs.php" style = "color: rgb(0,0,0)">Meatballs Recipe</a></li>
                             <li><a href="pancakes.php" style = "color: rgb(0,0,0)">Pancakes Recipe</a></li>
-                            <li><a href="login.html" style = "color: rgb(0,0,0)">Log In</a></li>
+                            <li><a href="login.php" style = "color: rgb(0,0,0)">Log In</a></li>
                         </ul>
                     </div>
                 </div>
